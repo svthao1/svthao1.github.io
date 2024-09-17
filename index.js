a6dda5ddcf52d5370c9eec8afccf9ee53bb9bf56
svthao1 <svthao01@gmail.com>
2024-09-16 20:53:09 -0400

    removed js index

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $.getJSON("google-books-search.json", function(data){
-            $(".title").html(data.title);
-            $(".authors").html(data.authors);
-    })
-
-})
